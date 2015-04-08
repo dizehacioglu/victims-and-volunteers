f51ef67a2fcb25d8ebb864a05525588e95aa8d3e
dizehacioglu <dh1272@nyu.edu>
2015-04-07 19:49:27 -0600

    Added volunteer prompts and alert listing

--- main.js
+++ main.js
@@ -16,3 +16,55 @@ for(var i = 0; i < victims; i++) {
 	victimPhones.push(victimPhone);
 	victimStreets.push(victimStreet);
 }
+
+// Repeat steps 1 and 2, but this time you are asking for volunteers.
+var volunteers = prompt("How many volunteers are there?", "ex. 3");
+
+var volunteerNames = [];
+var volunteerPhones = [];
+var volunteerStreets = [];
+
+for(var i = 0; i < volunteers; i++)
+{
+	var volunteerName = prompt("volunteer #" + (i+1) + " name:");
+	var volunteerPhone = prompt("volunteer #" + (i+1) + " phone:");
+	var volunteerStreet = prompt("volunteer #" + (i+1) + " street:");
+
+	volunteerNames.push(volunteerName);
+	volunteerPhones.push(volunteerPhone);
+	volunteerStreets.push(volunteerStreet);
+}
+
+
+// At the end, print out, in a single alert, the number of persons in need, the number of volunteers, and a list of all persons in need and all volunteers.
+var listVictims = "";
+var listVolunteers = "";
+
+for(var i = 0; i < victimNames.length; i++) {
+	listVictims += victimNames[i] + " ";
+}
+
+for(var i = 0; i < volunteerNames.length; i++) {
+	listVolunteers += volunteerNames[i] + " ";
+}
+
+
+alert(
+	"# of victims: " + victims  
+	+ "\n# of volunteers: " + volunteers
+	+ "\nVictims: " + listVictims
+	+ "\nVolunteers: " + listVolunteers
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
